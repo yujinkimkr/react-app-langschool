6b579da96fc47327b82a80322b1dfe651f5b8341
uz1ns <anjgksi73@naver.com>
2023-06-20 14:44:03 +0200

    added alt to img tag

--- src/components/UI/MemberCard.js
+++ src/components/UI/MemberCard.js
@@ -5,19 +5,19 @@ const MemberCard = (props) => {
   return <div className={classes.profile__container}>
     {
       props.profile === 'profile1' &&
-      <img className={classes.profile} src={require('../../images/profile1.svg').default} />
+      <img className={classes.profile} src={require('../../images/profile1.svg').default} alt='profile1' />
     }
     {
       props.profile === 'profile2' &&
-      <img className={classes.profile} src={require('../../images/profile2.svg').default} />
+      <img className={classes.profile} src={require('../../images/profile2.svg').default} alt='profile2' />
     }
     {
       props.profile === 'profile3' &&
-      <img className={classes.profile} src={require('../../images/profile3.svg').default} />
+      <img className={classes.profile} src={require('../../images/profile3.svg').default} alt='profile3' />
     }
     {
       props.profile === 'profile4' &&
-      <img className={classes.profile} src={require('../../images/profile4.svg').default} />
+      <img className={classes.profile} src={require('../../images/profile4.svg').default} alt='profile4' />
     }
 
     <p>{props.memberName}</p>
